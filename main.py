548dbc89ee078759e05e7a0eb8d2601bf59186e4
James Yan <james.yan.dev@gmail.com>
2024-04-27 13:17:42 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+from config import *
+from datetime import datetime
+import smtplib
+import time
+
+
+# Your position is within +5 or -5 degrees of the ISS position.
+def within_5():
+    #  ISS Details
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+    iss_data = iss_response.json()
+
+    iss_latitude = float(iss_data["iss_position"]["latitude"])
+    iss_longitude = float(iss_data["iss_position"]["longitude"])
+
+    if abs(MY_LAT - iss_latitude) <= 5 and abs(MY_LONG - iss_longitude) < 5:
+        return True
+
+
+#  Checks that the current hour is between sunrise and sunset in your location
+def hour_between():
+    #  Sunrise Details
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    sun_response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    sun_response.raise_for_status()
+    sun_data = sun_response.json()
+    sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+    current_hour = datetime.now().hour
+
+    return current_hour >= sunset or current_hour <= sunrise
+
+
+# Checks that cloud cover is low enough
+def low_cloud_cover():
+    parameters = {
+        "lat": MY_LAT,
+        "lon": MY_LONG,
+        "units": "metric",
+        "appid": API_KEY
+    }
+
+    cloud_response = requests.get("https://api.openweathermap.org/data/3.0/onecall", params=parameters)
+    cloud_response.raise_for_status()
+    cloud_data = cloud_response.json()
+    cloud_current = cloud_data["current"]["clouds"]
+    return int(cloud_current) <= 30
+
+
+#  Code
+while within_5() and hour_between() and low_cloud_cover():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_PASSWORD,
+            msg=f"Subject:ISS ALERT\n\nLook up, the ISS is about!"
+        )
+    time.sleep(60)
